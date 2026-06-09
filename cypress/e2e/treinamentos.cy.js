811b0f3be44588482672cfaa26edc13b0d9339dc
Tiago Duarte <thiagoduarteqa20@gmail.com>
2026-06-09 19:05:35 -0300

    adicao da base do projeto

---
+++ cypress/e2e/treinamentos.cy.js
@@ -0,0 +1,11 @@
+describe('Fluxo de Login', () => {
+
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('Deve realizar login com sucesso', () => {
+    // Implementação do teste
+  });
+
+});
